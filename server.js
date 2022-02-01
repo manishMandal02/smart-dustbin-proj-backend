bad533a72f820804ac8c4a71953b3e2fef6d781f
manishMandal02 <manishmandalj@gmail.com>
2022-02-02 02:33:35 +0530

    minor fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server is running...');
+  res.send('Server is running..');
 });
 
 app.use('/webhooks', routes);
