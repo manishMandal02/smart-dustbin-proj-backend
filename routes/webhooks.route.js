66581d489e80502b33e61e8739434a0e2889ab75
manishMandal02 <manishmandalj@gmail.com>
2022-02-02 02:58:12 +0530

    try fix 03

--- routes/webhooks.route.js
+++ routes/webhooks.route.js
@@ -9,8 +9,8 @@ router.get('/get', (req, res) => {
   res.status(200);
   res.json({
     message: 'success',
-    dryDustbinPercentage: dryDustbinPercentage,
-    wetDustbinPercentage: wetDustbinPercentage,
+    dryDustbinPercentage: dryDustbinPercentage.toString(),
+    wetDustbinPercentage: wetDustbinPercentage.toString(),
   });
 });
 
