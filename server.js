acee8e3410d7927250a9f043c43464cf5dd172b5
manishMandal02 <manishmandalj@gmail.com>
2022-02-02 03:22:22 +0530

    try fix 09

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+
+const http = require('http');
+
 const cors = require('cors');
 const routes = require('./routes/webhooks.route');
 
 const app = express();
 
+dotenv.config();
+
 app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running..');
+app.get('/', (req, res, next) => {
+  res.send('Server is running');
 });
 
 app.use('/webhooks', routes);
 
+const server = http.createServer(app);
+
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));
