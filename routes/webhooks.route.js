c68685c231dd16204a65674d747f61d64a79b644
manishMandal02 <manishmandalj@gmail.com>
2022-02-02 03:17:07 +0530

    try fix 08

--- routes/webhooks.route.js
+++ routes/webhooks.route.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-let dryDustbinPercentage = 0;
-let wetDustbinPercentage = 0;
+let dryDustbinPercentage = 1;
+let wetDustbinPercentage = 1;
 
 router.get('/get', (req, res) => {
   res.status(200);
